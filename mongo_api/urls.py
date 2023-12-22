fbcedd87b4357e3a04b779c9091b97f0e7fe13b8
Deivanai Subramanian <deivanai.subramanian@iopex.com>
2023-12-22 18:12:57 +0530

    First Commit

--- mongo_api/urls.py
+++ mongo_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mongo_connect project.
+URL configuration for mongo_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
